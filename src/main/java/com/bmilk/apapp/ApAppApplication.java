0ec06e8b562eb62b1b2f3b6c13fd11d7768daf4f
xiaodoujiang <1426703092@qq.com>
2022-11-15 16:40:51 +0800

    init

---
+++ src/main/java/com/bmilk/apapp/ApAppApplication.java
@@ -0,0 +1,13 @@
+package com.bmilk.apapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApAppApplication.class, args);
+    }
+
+}
