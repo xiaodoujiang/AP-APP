dc73d45cbfe1c6330fbb265cefd3934a5de1bbbd
xiaodoujiang <1426703092@qq.com>
2022-11-18 21:02:17 +0800

    eureka server init

---
+++ eureka-server/src/main/java/com/bmilk/erureka/Eureka.java
@@ -0,0 +1,15 @@
+package com.bmilk.erureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Eureka {
+    public static void main(String[] args) {
+
+        SpringApplication.run(Eureka.class, args);
+    }
+}
